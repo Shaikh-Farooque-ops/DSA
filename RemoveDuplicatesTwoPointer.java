25ebe2d059ea08b0cea02b4422b823dc882ae9b5
Farooque <farooqueshaikh7499@gmail.com>
2025-10-02 23:20:39 +0530

    Update RemoveDuplicates files

--- RemoveDuplicatesTwoPointer.java
+++ RemoveDuplicatesTwoPointer.java
@@ -3,27 +3,27 @@ package com.dsa.array;
 public class RemoveDuplicatesTwoPointer {
 
 	public static void main(String[] args) {
-
 		int[] nums = {1,1,2};
 		System.out.println(removeDuplicates(nums));
-
 	}
 
 	public static int removeDuplicates(int[] nums)
 	{
 		int i=0;
-
 		for(int j=1;j<nums.length;j++)
 		{
-
-			if(nums[i]!=nums[j])
+			// if j is unique with i then proceed
+			if(nums[j]!=nums[i])		
 			{
+				// placed j +1 index of i
+				nums[i+1]=nums[j];
+				// moving also to store
 				i++;
-				nums[i]=nums[j];
 			}
 		}
 
+		// i index is 1 but the size is 2 so increment+1
 		return i+1;
-	}
 
+	}
 }
--- RemoveElementsTwoPointer.java
+++ RemoveElementsTwoPointer.java
@@ -3,22 +3,20 @@ package com.dsa.array;
 public class RemoveElementsTwoPointer {
 
 	public static void main(String[] args) {
-		
-		int[] num = {3,2,2,3};	
-		int value= 3;
+		int[] num = {0,1,2,2,3,0,4,2};	
+		int value= 2;
 		System.out.println(removeElement(num, value));
 	}
 	
 	public static int removeElement(int[] num, int value)
 	{
 		int i=0;
-		
 		for(int j=0;j<num.length;j++)
 		{
 			if(num[j]!=value)
 			{
-				i++;
 				num[i] = num[j];
+				i++;
 			}
 		}
 		
